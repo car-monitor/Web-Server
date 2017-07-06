533c8238968de3dd563cc6c68c252c6638d22e81
singleboat <sysu_xzz@163.com>
2017-07-06 17:37:06 +0800

    add cookie check

--- handler/car.py
+++ handler/car.py
@@ -5,48 +5,64 @@ import model.Car as Car
 
 class RegisterHandler(BaseHandler):
 	def post(self):
-		param = self.request.body.decode('utf-8')
-		data = json.load(param)
-		car = Car(self.db)
-		if car.retrieve({'carPlate': data['carPlate']}) == None:
-			carInfo = car.create(data)
-			self.write({'status': 1, 'car': carInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
 		else:
-			self.write({'status': 0})
+			param = self.request.body.decode('utf-8')
+			data = json.load(param)
+			car = Car(self.db)
+			if car.retrieve({'carPlate': data['carPlate']}) == None:
+				carInfo = car.create(data)
+				self.write({'status': 1, 'car': carInfo})
+			else:
+				self.write({'status': 0})
 
 
 class RemoveHandler(BaseHandler):
 	def post(self):
-		param = self.request.body.decode('utf-8')
-		data = json.load(param)
-		car = Car(self.db)
-		car.delete(data)
-		self.write({'status': 1})
+		if not self.current_user:
+			self.set_status(401)
+			return
+		else:
+			param = self.request.body.decode('utf-8')
+			data = json.load(param)
+			car = Car(self.db)
+			car.delete(data)
+			self.write({'status': 1})
 
 
 class ModifyHandler(BaseHandler):
 	def post(self):
-		param = self.request.body.decode('utf-8')
-		data = json.load(param)
-		car = Car(self.db)
-		carInfo = car.update(data)
-		if carInfo != None:
-			self.write({'status': 1, 'car': carInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
 		else:
-			self.write({'status': 0})
+			param = self.request.body.decode('utf-8')
+			data = json.load(param)
+			car = Car(self.db)
+			carInfo = car.update(data)
+			if carInfo != None:
+				self.write({'status': 1, 'car': carInfo})
+			else:
+				self.write({'status': 0})
 
 
 class GetcarHandler(BaseHandler):
 	def get(self):
-		carid = self.get_argument('id', '')
-		car = Car(self.db)
-		carInfo = car.retrieve({'id': carid})
-		if carInfo != None:
-			order = Order(self.db)
-			ordersInfo = order.retrieve({'carID': car['id']})
-			self.write({'status': 1, 'car': carInfo, 'order': ordersInfo)
+		if not self.current_user:
+			self.set_status(401)
+			return
 		else:
-			self.write({'status': 0})
+			carid = self.get_argument('id', '')
+			car = Car(self.db)
+			carInfo = car.retrieve({'id': carid})
+			if carInfo != None:
+				order = Order(self.db)
+				ordersInfo = order.retrieve({'carID': car['id']})
+				self.write({'status': 1, 'car': carInfo, 'order': ordersInfo)
+			else:
+				self.write({'status': 0})
 
 
 class GetcarsHandler(BaseHandler):
--- handler/company.py
+++ handler/company.py
@@ -6,57 +6,81 @@ import model.Unit as Unit
 
 class GetallunitsHandler(BaseHandler):
 	def get(self):
-		unit = Unit(self.db)
-		unitsInfo = unit.retrieve()
-		self.write({'status': 1, 'units': unitsInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
+		else:
+			unit = Unit(self.db)
+			unitsInfo = unit.retrieve()
+			self.write({'status': 1, 'units': unitsInfo})
 
 
 class GetunitHandler(BaseHandler):
 	def get(self):
-		unit = Unit(self.db)
-		unitid = self.get_argument('id', '')
-		unitInfo = unit.retrieve({'id': unitid})
-		if unitInfo != None:
-			self.write({'status': 1, 'unit': unitInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
 		else:
-			self.write({'status': 0})
+			unit = Unit(self.db)
+			unitid = self.get_argument('id', '')
+			unitInfo = unit.retrieve({'id': unitid})
+			if unitInfo != None:
+				self.write({'status': 1, 'unit': unitInfo})
+			else:
+				self.write({'status': 0})
 
 
 class RegisterunitHandler(BaseHandler):
 	def post(self):
-		param = self.request.body.decode('utf-8')
-		data = json.load(param)
-		unit = Unit(self.db)
-		if unit.retrieve({'name': data['name']}) == None:
-			unitInfo = unit.create(data)
-			self.write({'status': 1, 'unit': unitInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
 		else:
-			self.write({'status': 0})
+			param = self.request.body.decode('utf-8')
+			data = json.load(param)
+			unit = Unit(self.db)
+			if unit.retrieve({'name': data['name']}) == None:
+				unitInfo = unit.create(data)
+				self.write({'status': 1, 'unit': unitInfo})
+			else:
+				self.write({'status': 0})
 
 class GetalldepartmentHandler(BaseHandler):
 	def get(self):
-		department = Department(self.db)
-		departmentsInfo = department.retrieve()
-		self.write({'status': 1, 'departments': departmentsInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
+		else:
+			department = Department(self.db)
+			departmentsInfo = department.retrieve()
+			self.write({'status': 1, 'departments': departmentsInfo})
 
 
 class GetdepartmentHandler(BaseHandler):
 	def get(self):
-		department = Department(self.db)
-		departmentid = self.get_argument('id', '')
-		departmentInfo = department.retrieve({'id': departmentid})
-		if departmentInfo != None:
-			self.write({'status': 1, 'department': departmentInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
 		else:
-			self.write({'status': 0})
+			department = Department(self.db)
+			departmentid = self.get_argument('id', '')
+			departmentInfo = department.retrieve({'id': departmentid})
+			if departmentInfo != None:
+				self.write({'status': 1, 'department': departmentInfo})
+			else:
+				self.write({'status': 0})
 
 class RegisterdeparmentHandler(BaseHandler):
 	def post(self):
-		param = self.request.body.decode('utf-8')
-		data = json.load(param)
-		department = Department(self.db)
-		if department.retrieve({'name': data['name']}) == None:
-			departmentInfo = department.create(data)
-			self.write({'status': 1, 'department': departmentInfo})
+		if not self.current_user:
+			self.set_status(401)
+			return
 		else:
-			self.write({'status': 0})
+			param = self.request.body.decode('utf-8')
+			data = json.load(param)
+			department = Department(self.db)
+			if department.retrieve({'name': data['name']}) == None:
+				departmentInfo = department.create(data)
+				self.write({'status': 1, 'department': departmentInfo})
+			else:
+				self.write({'status': 0})
