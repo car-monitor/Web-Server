3803c648495c7ed576015598eb62a07bc0cada8b
438841268@qq.com <438841268@qq.com>
2017-07-07 02:03:03 +0800

    添加部分user操作

--- handler/base.py
+++ handler/base.py
@@ -7,6 +7,24 @@ class BaseHandler(tornado.web.RequestHandler):
 		# 获取数据库连接
         return self.application.db
 
+    @property
+    def body(self):
+        param = self.request.body.decode('utf-8')
+		jparam = json.load(param)
+        return jparam
+
+    # 判断是否有权限
+    def authority(self, operate, operated):
+
+        jparam = self.body()
+
+        if not operate or not operated:
+            return False
+
+        if operate['authority'] == 2 or operate['authority'] == 3 and operated['authority'] < operate['authority']:
+            return True
+        return False
+
 	def get_current_user(self):
 		# 通过cookie获取当前用户
 		user_id = self.get_secure_cookie('id')
--- handler/user.py
+++ handler/user.py
@@ -6,12 +6,16 @@ class RegisterHandler(BaseHandler):
 
 	def post(self):
 		#将body读取出来并转换为dict对象
-		param = self.request.body.decode('utf-8')
-		jparam = json.load(param)
+		jparam = self.body
 		usermodel = userModel(self.db)
 
+		# 账号密码不能为空
+		if not jparam['username'] or not jparam['password']:
+			self.write({"status" : 0})
+			return
+
 		# 判断用户名是否已存在，已存在则注册失败
-		if not usermodel.retrieve(jparam):
+		if not usermodel.retrieve(jparam.items()[0]):
 			usermodel.create(jparam)
 			self.write({"status" : 1})
 		else:
@@ -21,30 +25,117 @@ class LoginHandler(BaseHandler):
 
 	def post(self):
 		#将body读取出来并转换为dict对象
-		param = self.request.body.decode('utf-8')
-		jparam = json.load(param)
+		jparam = self.body
 		usermodel = userModel(self.db)
+
+		# 账号密码不能为空
+		for key in jparam:
+			if jparam[key] == '':
+				self.write({"status" : 0})
+				return
 
 		#判断用户是否存在,若不存在则报错
 		user = usermodel.retrieve(jparam)
 		if not user:
 			self.write({"status" : 0})
 			return
-		if user['password'] == jparam['password']:
-			self.set_secure_cookie('id', user['id'])
-			self.write({"status" : 1, "user" : user})
+		self.set_secure_cookie('id', user['id'])
+		self.write({"status" : 1, "user" : user})
+
+class RegisterinfoHandler(BaseHandler):
+
+	def post(self):
+		#将body读取出来并转换为dict对象
+		jparam = self.body
+		usermodel = userModel(self.db)
+
+		#未登录不能进行操作
+		if not self.current_user:
+			self.set_status(401)
+			return
+
+		#性别不能为空
+		if self.current_user == jparam['id'] and jparam['sex'] != '':
+			user = usermodel.retrieve({'id': jparam['id']})
+			#不能重复注册
+			if user.has_key('sex'):
+				self.write({"status" : 0})
+			else:
+				jparam.update({'authority': 0})
+				usermodel.update(jparam)
+				self.write({"status" : 1})
+		else:
+			self.write({"status" : 0})
 
 
 
 
 class LogoutHandler(BaseHandler):
-	pass
+
+	def get(self):
+		if self.current_user.has_key('id'):
+			self.clear_cookie('id')
+		self.write({"status": 1})
+
 
 class ModifyinfoHandler(BaseHandler):
-	pass
+
+	def post(self):
+		jparam = self.body
+		usermodel = userModel(self.db)
+
+		#未登录不能进行操作
+		if not self.current_user:
+			self.set_status(401)
+			return
+
+		#不能修改权限
+		if jparam.has_key('authority'):
+			self.write({"status" : 0})
+			return
+
+		#判断是否有修改权限
+		if not self.current_user == jparam['id']:
+			operate = usermodel.retrieve({'id': self.current_user})
+			operated = usermodel.retrieve({'id': jparam['id']})
+			if not self.authority(operate, operated):
+				self.write({"status" : 0})
+				return
+
+		#需要修改的项不能为空
+		for key in jparam:
+			if jparam[key] == '':
+				self.write({"status" : 0})
+
+		usermodel.update(jparam)
+		self.write({"status" : 1, "user": usermodel.retrieve({'id': jparam['id']})})
 
 class ModifyauthorityHandler(BaseHandler):
-	pass
+
+	def post(self):
+		jparam = self.body
+		usermodel = userModel(self.db)
+
+		#未登录不能进行操作
+		if not self.current_user:
+			self.set_status(401)
+			return
+
+		#判断是否有权限修改
+		operate = usermodel.retrieve({'id': self.current_user})
+		operated = usermodel.retrieve({'id': jparam['id']})
+		if not self.authority(operate, operated):
+			self.write({"status" : 0})
+			return
+
+		#只修改权限
+		if not jparam.has_key('authority'):
+			self.write({"status" : 0})
+			return
+		a = {'authority': jparam['authority']}
+		usermodel.update(a)
+		self.write({"status" : 1})
+
 
 class GetusersHandler(BaseHandler):
 	pass
