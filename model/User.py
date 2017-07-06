be4877498fb349c1f5a111fbc29df05dfa779c2c
singleboat <sysu_xzz@163.com>
2017-07-06 16:31:08 +0800

    更改User.py的retrieve方法

--- model/User.py
+++ model/User.py
@@ -13,20 +13,16 @@ class User:
 		return self.user_collection.insert({'username': data['username'], 'password': data['password']})
 
 	def retrieve(self, data):
-		if data.has_key('authority'):
-			#根据权限查找用户时调用
-			users = self.user_collection.find({'authority': data['authority']})
-			for user in users:
-				user['id'] = str(user['_id'])
-				del user['_id']
+		users = self.user_collection.find(data)
+		for user in users:
+			user['id'] = str(user['_id'])
+			del user['_id']
+		if len(users) == 0: 
+			return None
+		else if len(users) == 1: 
+			return users[0]
+		else:
 			return users
-		else: 
-			#验证用户名是否存在时调用
-			user = self.user_collection.find_one({'username': data['username']})
-			if user != None:
-				user['id'] = str(user['_id'])
-				del user['_id']
-			return user
 
 	def update(self, data):
 		user = self.user_collection.find_one({'_id': ObjectId(data['id'])})
